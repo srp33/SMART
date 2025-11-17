eb6354b6fd0ad1fbd168acc7df9eb224428df05a
Stephen Piccolo <stephen.piccolo.byu@gmail.com>
2025-11-17 15:04:49 -0700

    Separated build logic

--- docker-run.sh
+++ docker-run.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-docker stop pdf-annotator
-docker system prune -f
-
-docker build -t pdf-annotator:latest .
-
-if [ $? -eq 0 ]; then
-    echo "✓ Docker image built successfully!"
-    echo "Run with: ./docker-run.sh"
-else
-    echo "✗ Docker build failed"
-    exit 1
-fi
-
 echo "Starting PDF Annotator container..."
 docker run -d \
     --name pdf-annotator \
